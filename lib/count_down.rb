8da084d268667034c1e361397a0676be5bf994a6
Sayana <nisayana23@yandex.ru>
2020-07-14 18:43:10 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -20,10 +20,10 @@
 #   puts "I am the #{count}, I love to count!"
 #   count = count + 1
 # end
-exit_num = 0
+
 count = 10
-while count < 10 do
-  break if count == exit_num
+while count >=1 do
   puts count
-  count = count - 1
+  count -= 1
 end
+puts "Happy New Year!"
